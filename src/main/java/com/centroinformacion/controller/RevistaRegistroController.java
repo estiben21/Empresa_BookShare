b577e146ec5b27e76c0502b8af6af67da66717e1
estiben21 <estibenyerovim@gmail.com>
2023-09-16 11:39:03 -0500

    inicio de registro revista

---
+++ src/main/java/com/centroinformacion/controller/RevistaRegistroController.java
@@ -0,0 +1,13 @@
+package com.centroinformacion.controller;
+
+/**
+ * @author SUITE
+ */
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RevistaRegistroController {
+
+	
+	
+}
