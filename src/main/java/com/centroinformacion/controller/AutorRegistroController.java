fd89459bacab881d45c4ba20d9a506a35a2ecd92
7ionel <lionelorihuelac@gmail.com>
2023-09-16 11:41:09 -0500

    Inicio de registro de Autor

---
+++ src/main/java/com/centroinformacion/controller/AutorRegistroController.java
@@ -0,0 +1,12 @@
+package com.centroinformacion.controller;
+
+/**
+ * @author Lionel Orihuela
+ */
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class AutorRegistroController {
+
+}
