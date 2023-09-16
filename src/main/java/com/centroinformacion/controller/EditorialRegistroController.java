3a29a07a138bbdf78e89deeb7828ea3eb02f132f
lesliemarlo <lesliemarlo09@gmail.com>
2023-09-16 11:39:12 -0500

    Unicio de registro de editorial

---
+++ src/main/java/com/centroinformacion/controller/EditorialRegistroController.java
@@ -0,0 +1,16 @@
+package com.centroinformacion.controller;
+
+/**
+ * @author Leslie Marlo
+ */
+import org.springframework.stereotype.Controller;
+
+
+
+
+@Controller
+public class EditorialRegistroController {
+
+
+	}
+
---
+++ src/main/java/com/centroinformacion/repository/EditorialRepository.java
@@ -0,0 +1,15 @@
+package com.centroinformacion.repository;
+/**
+ * 
+ * @author Leslie Marlo
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface EditorialRepository {
+	
+	
+
+}
