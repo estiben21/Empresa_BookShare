5377ba874b584b59be9b4c0b2246b099fb1e5354
Nando21MS <manriquef400@gmail.com>
2023-09-16 11:39:03 -0500

    Inicio de Registro Libro

---
+++ src/main/java/com/centroinformacion/controller/LibroRegistroController.java
@@ -0,0 +1,11 @@
+package com.centroinformacion.controller;
+
+/**
+ *  @author Fernando Manrique Solano
+ */
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LibroRegistroController {
+	
+}
