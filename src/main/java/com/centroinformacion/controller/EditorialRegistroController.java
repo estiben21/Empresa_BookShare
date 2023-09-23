126e32f733ff77050cf0ce5bcc89c48b221ff552
lesliemarlo <lesliemarlo09@gmail.com>
2023-09-23 00:40:40 -0500

    Avance - Leslie Marlo

--- src/main/java/com/centroinformacion/controller/EditorialRegistroController.java
+++ src/main/java/com/centroinformacion/controller/EditorialRegistroController.java
@@ -1,16 +1,64 @@
 package com.centroinformacion.controller;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 /**
  * @author Leslie Marlo
  */
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.centroinformacion.entity.Editorial;
+import com.centroinformacion.entity.Usuario;
+import com.centroinformacion.service.EditorialService;
+import com.centroinformacion.util.AppSettings;
 
 
-
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class EditorialRegistroController {
+	@Autowired
+	private EditorialService editorialService;
+	
+	@PostMapping("/registraEditorial")
+	@ResponseBody
+	public Map<?, ?> registra(Editorial obj, HttpSession session){
+		Usuario objUsuario = (Usuario)session.getAttribute("objUsuario");
+		//datos automaticos
+		obj.setFechaActualizacion(new Date());
+		obj.setFechaRegistro(new Date());
+		obj.setEstado(AppSettings.ACTIVO);
+		obj.setUsuarioRegistro(objUsuario);
+		obj.setUsuarioActualiza(objUsuario);
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		Editorial objSalida = editorialService.insertaActualizaEditorial(obj);
+		if (objSalida == null) {
+			map.put("MENSAJE", "Error en el registro");
+		}else {
+			map.put("MENSAJE", "Registro exitoso");
+		}
+		return map;
+	}
 
-
+	//Buscar RUC
+	@GetMapping("/buscaPorRucEditorial")
+	@ResponseBody
+	public String validaDNI(String ruc) {
+		List<Editorial> lstEditorial = editorialService.listaPorRuc(ruc);
+		if (CollectionUtils.isEmpty(lstEditorial)) {
+			return "{\"valid\" : true }";
+		} else {
+			return "{\"valid\" : false }";
+		}
+	}
 	}
 
