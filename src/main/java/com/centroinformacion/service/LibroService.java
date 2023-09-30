2a7c9e20193a0098f5d7610da14782d83f4cb253
Nando21MS <manriquef400@gmail.com>
2023-09-30 12:52:46 -0500

    SEMANA06

--- src/main/java/com/centroinformacion/service/LibroService.java
+++ src/main/java/com/centroinformacion/service/LibroService.java
@@ -1,8 +1,11 @@
 package com.centroinformacion.service;
 
+import java.util.List;
+
 import com.centroinformacion.entity.Libro;
 
 public interface LibroService {
 	public abstract Libro insertaActualizaLibro(Libro obj);
 	public abstract boolean existeLibroConSerie(String serie);
+	public abstract List<Libro> listaPorTituloLike(String filtro);
 }
