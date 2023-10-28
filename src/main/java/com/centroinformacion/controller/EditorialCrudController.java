422c792e80a38e0d8b785a66ff5e4dc699d69da0
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-10-28 00:42:39 -0500

    ajustes

--- src/main/java/com/centroinformacion/controller/EditorialCrudController.java
+++ src/main/java/com/centroinformacion/controller/EditorialCrudController.java
@@ -11,14 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.centroinformacion.entity.Editorial;
 import com.centroinformacion.entity.Usuario;
 import com.centroinformacion.service.EditorialService;
 import com.centroinformacion.util.AppSettings;
-
 
 import jakarta.servlet.http.HttpSession;
 
