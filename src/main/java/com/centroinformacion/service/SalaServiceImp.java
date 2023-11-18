fe20680b921385dacac6954d832e3bc0f673623b
MARCO-viera <marcosamsung942@gmail.com>
2023-11-18 13:30:22 -0500

    PC3-TERMINADO - CONSULTA SALA

--- src/main/java/com/centroinformacion/service/SalaServiceImp.java
+++ src/main/java/com/centroinformacion/service/SalaServiceImp.java
@@ -1,4 +1,5 @@
 package com.centroinformacion.service;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -7,18 +8,18 @@ import org.springframework.stereotype.Service;
 
 import com.centroinformacion.entity.Sala;
 import com.centroinformacion.repository.SalaRepository;
+
 @Service
 public class SalaServiceImp implements SalaService {
-	@Autowired
+	@Autowired	
 	private SalaRepository repository;
-	@Override
-	public List<Sala> listaTodos() {
-		return repository.findByOrderByNumeroAsc();
-	}
-
+	
 	@Override
 	public Sala insertaActualizaSala(Sala obj) {
-		
+		return repository.save(obj);
+	}
+	@Override
+	public Sala insertaRegistraSala(Sala obj) {
 		return repository.save(obj);
 	}
 
@@ -26,53 +27,31 @@ public class SalaServiceImp implements SalaService {
 	public List<Sala> listaPorNumero(String numero) {
 		return repository.findByNumero(numero);
 	}
-
-	
-
 	
 	@Override
-	public Optional<Sala> buscaSala(int idSala) {
+	public List<Sala> listPorNumerolike(String filtro) {
+		return repository.listaPorNumeroLike(filtro);
+	}
+	@Override
+	public Optional<Sala> buscarSala(int idSala) {
 		return repository.findById(idSala);
 	}
-
 	@Override
 	public List<Sala> listaPorNumeroIgualRegistra(String numero) {
 		return repository.listaPorNumeroIgualRegistra(numero);
+
 	}
 
+	
 	@Override
-	public List<Sala> listaPorNumeroIgualActualiza(String numero, int id) {
-		return repository.listaPorNumeroIgualActualiza(numero, id);
-	}
-
-	@Override
-	public List<Sala> listaPorNumeros(String numero) {
-		return repository.findByNumeroIgnoreCase(numero);
+	public List<Sala> listaPorNumeroIgualActualiza(String numero) {
+		return repository.listaPorNumeroIgualActualiza(numero);
 	}
 	@Override
-	public List<Sala> listaPorNumeroLike(String filtro) {
-		// TODO Auto-generated method stub
-		return repository.listaPorNumeroLike(filtro);
-	}
-
-	@Override
-	public List<Sala> listaConsultaSala(int estado, 
-	        String numero,
-	        int piso,
-	        int numAlumnos,
-	        String recursos,
-	        int idSede,
-	        int idTipoSala) {
+	public List<Sala> listaConsultaSala(int estado, String numero, int piso, int numAlumnos, String recursos,
+			int idSede, int idTipoSala) {
 		return repository.listaConsultaSala(estado, numero, piso, numAlumnos, recursos, idSede, idTipoSala);
 	}
-
-	
-	
-
-	
-
-	
-
 	
 
 }
